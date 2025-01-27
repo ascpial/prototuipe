b152c49837c825ed7eace867d053f3938b06751f
ascpial <mail@ascpial.fr>
2025-01-27 14:51:59 +0100

    Try to improve performance

--- rollup.sh
+++ rollup.sh
@@ -1,2 +1,2 @@
-npx rollup -p @rollup/plugin-node-resolve m3bundle.js -o site/m3bundle.js
-npx rollup -p @rollup/plugin-node-resolve utils.js -o site/utils.js
+npx rollup -p @rollup/plugin-node-resolve -p @rollup/plugin-terser m3bundle.js -o site/m3bundle.js
+npx rollup -p @rollup/plugin-node-resolve -p @rollup/plugin-terser utils.js -o site/utils.js
