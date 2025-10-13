5397741a9ee348c1285ec7426c61d43daee07a0e
ascpial <mail@ascpial.fr>
2025-10-13 15:40:47 +0200

    Fix the fix

--- site/code.js
+++ site/code.js
@@ -379,7 +379,7 @@ let select = {
             fg: screen.fgColor,
             bg: screen.bgColor,
           }
-          if (x == 0 || y == 0 || x == screen.size.height - 1 || y == screen.size.width - 1) {
+          if (x == 0 || y == 0 || x == screen.size.width - 1 || y == screen.size.height - 1) {
             screen.drawChar(32, x, y, screen.fgColor, screen.bgColor);
           }
         }
